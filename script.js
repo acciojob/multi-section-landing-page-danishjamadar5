0e83f1c20303cbbed08267a0200c5f6337654031
danishjamadar5 <117918682+danishjamadar5@users.noreply.github.com>
2023-10-01 13:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+            const form = document.getElementById('newsletter-form');
+            const emailInput = document.getElementById('email');
+            const message = document.getElementById('message');
+
+            form.addEventListener('submit', function (e) {
+                e.preventDefault();
+                const email = emailInput.value.trim();
+
+                if (!isValidEmail(email)) {
+                    message.textContent = 'Please enter a valid email address.';
+                    message.style.color = 'red';
+                    return;
+                }
+
+                // Here, you can send the email to your server or perform other actions.
+                // For now, just display a success message.
+                message.textContent = 'Thank you for subscribing!';
+                message.style.color = 'green';
+
+                // Clear the input field
+                emailInput.value = '';
+            });
+
+            function isValidEmail(email) {
+                const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                return emailRegex.test(email);
+            }
+        });
